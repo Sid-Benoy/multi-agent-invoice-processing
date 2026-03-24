d75a3c78fd912d2a1e94073ff54de50080bd8045
sid <siddharth.benoy@utexas.edu>
2026-03-24 12:36:11 -0500

    got rid of api key

--- data/main.py
+++ data/main.py
@@ -6,7 +6,6 @@ API_KEY = os.environ.get("XAI_API_KEY")
 if not API_KEY:
     raise ValueError("Set XAI_API_KEY in your environment before running this script.")
 
-API_KEY = 'xai-PKmRa4frqly5uaztFQIz4q0jj4bzGcHECiN2yISN7QVkAhHrNwxJlVqIoUEdQNE3liWkilQLG9R2jiIk'
 
 client = Grok(api_key=API_KEY)
 response = client.chat.completions.create(
